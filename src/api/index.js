6bb490940761d2f0e84973cee7ea96d285560f16
陈依婷 <1534136525@qq.com>
2020-03-30 13:38:37 +0800

    chore：删除无用注释代码

--- src/api/index.js
+++ src/api/index.js
@@ -2,21 +2,11 @@
  * @Author: chenyt 
  * @Date: 2020-03-22 12:02:58 
  * @Last Modified by: chenyt
- * @Last Modified time: 2020-03-27 18:48:00
+ * @Last Modified time: 2020-03-30 13:37:26
  */
 import * as base from './modules/base'
 import * as common from './modules/common'
 import * as user from './modules/user'
-
-// const apiContext = require.context('./modules', true, /\.js$/)
-// apiContext.keys().forEach(api => {
-//   const apiConfig = apiContext(api)
-//   /**
-//   * 兼容 import export 和 require module.export 两种规范
-//   */
-//   const ctrl = apiConfig.default || apiConfig
-//   export ctrl
-// })
 export default {
   base,
   common,
