f1813d94ca6b316512101376108aad2c3b364df8
陈依婷 <1534136525@qq.com>
2020-04-01 10:38:16 +0800

    fix:修改分片配置位置错误问题

--- webpack.config.js
+++ webpack.config.js
@@ -7,5 +7,28 @@ module.exports = {
   },
   plugins: [
     new webpack.HotModuleReplacementPlugin(), //热更新插件
-  ]
+  ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all', // 控制webpack选择哪些代码块用于分割（其他类型代码块按默认方式打包）。有3个可选的值：initial、async和all。
+      minSize: 30000, // 形成一个新代码块最小的体积
+      maxSize: 0,
+      minChunks: 2, // 在分割之前，这个代码块最小应该被引用的次数（默认配置的策略是不需要多次引用也可以被分割）
+      maxAsyncRequests: 5, // 按需加载的代码块，最大数量应该小于或者等于5
+      maxInitialRequests: 3, // 初始加载的代码块，最大数量应该小于或等于3
+      automaticNameDelimiter: '~',
+      name: true,
+      cacheGroups: {
+        vendors: { // 将所有来自node_modules的模块分配到一个叫vendors的缓存组
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10 // 缓存组的优先级(priotity)是负数，因此所有自定义缓存组都可以有比它更高优先级
+        },
+        default: {
+          minChunks: 2, // 所有重复引用至少两次的代码，会被分配到default的缓存组。
+          priority: -20, // 一个模块可以被分配到多个缓存组，优化策略会将模块分配至跟高优先级别（priority）的缓存组
+          reuseExistingChunk: true // 允许复用已经存在的代码块，而不是新建一个新的，需要在精确匹配到对应模块时候才会生效。
+        }
+      }
+    }
+  }
 };
