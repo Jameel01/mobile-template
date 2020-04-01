5909f5d113a72a3812b7078ea5ed957136753fcd
陈依婷 <1534136525@qq.com>
2020-04-01 20:46:32 +0800

    feat:修改单位为vw和vh

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    首页1aaaaa111111111
+  <div class="test">
+    <p>海南省高校毕业生报到及未就业实名登记须知</p>
     <y-title content="标题"></y-title>
   </div>
 </template>
@@ -10,3 +10,14 @@ export default {
 
 }
 </script>
+<style lang='scss' scoped>
+.test {
+  p {
+    font-size: 32px;
+    padding:34px 54px;
+    color:#333;
+    font-weight: bold;
+  }
+}
+</style>
+
