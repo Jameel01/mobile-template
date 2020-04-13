f36e4f0d24c7aef81c70de0dc2b1fdb3262d3c52
陈依婷 <1534136525@qq.com>
2020-04-13 16:39:12 +0800

    chore:格式缩进规范

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,8 +1,8 @@
 <template>
-    <div class="test">
-        <p>海南省高校毕业生报到及未就业实名登记须知</p>
-        <y-title content="标题"></y-title>
-    </div>
+  <div class="test">
+    <p>海南省高校毕业生报到及未就业实名登记须知</p>
+    <y-title content="标题"></y-title>
+  </div>
 </template>
 
 <script>
@@ -24,12 +24,12 @@ export default {
 </script>
 <style lang='scss' scoped>
 .test {
-    p {
-        font-size: 32px;
-        padding: 34px 54px;
-        color: #333;
-        font-weight: bold;
-    }
+  p {
+    font-size: 32px;
+    padding: 34px 54px;
+    color: #333;
+    font-weight: bold;
+  }
 }
 </style>
 
