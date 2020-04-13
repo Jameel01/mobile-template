4c28725f0e0bc4214052688cb9825df241f80dcb
abigcivet <949710612@qq.com>
2020-04-13 10:24:14 +0800

    refactor: 将webpack.config..js配置移动至vue.config.js统一配置

--- babel.config.js
+++ babel.config.js
@@ -3,18 +3,18 @@
  * @Autor: guoruliang
  * @Date: 2020-04-03 15:28:36
  * @LastEditors: guoruliang
- * @LastEditTime: 2020-04-09 17:03:44
+ * @LastEditTime: 2020-04-10 15:16:20
  */
 
 module.exports = {
-    presets: [
-        '@vue/app'
-    ],
-    plugins: [
-        ['import', {
-            libraryName: 'vant',
-            libraryDirectory: 'es',
-            style: true
-        }, 'vant']
-    ]
+  presets: [
+    "@vue/app"
+  ],
+  plugins: [
+    ["import", {
+      libraryName: "vant",
+      libraryDirectory: "es",
+      style: true
+    }, "vant"]
+  ]
 }
--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   plugins: {
-    'autoprefixer': {
+    "autoprefixer": {
       // browsers: ['Android >= 4.0', 'iOS >= 7']
     },
-    'postcss-pxtorem': {
-      rootValue:37.5,
-      propList: ['*'],
-      selectorBlackList  : ['van'], // 忽略转换正则匹配项
+    "postcss-pxtorem": {
+      rootValue: 37.5,
+      propList: ["*"],
+      selectorBlackList: ["van"] // 忽略转换正则匹配项
     }
   }
 }
