b41f847dc8b7d5479aa204771fbed7862ad19ada
郑小娜 <764427743@qq.com>
2020-04-22 16:43:18 +0800

    feat:新增ui规范路由，从公共路由中分离

--- src/router/common/index.js
+++ src/router/common/index.js
@@ -21,46 +21,7 @@ export default [
     name: "not-found",
     component: () => import(/* webpackChunkName: "notFoundModule" */"@/views/404.vue")
   },
-  {
-    path: "/ui-reset/data-entry/button",
-    name: "button",
-    component: () => import(/* webpackChunkName: "exceptionModule" */"@/views/ui-reset/data-entry/button/index.vue")
-  },
-  {
-    path: "/ui-reset/data-entry/checkbox",
-    name: "checkbox",
-    component: () => import(/* webpackChunkName: "exceptionModule" */"@/views/ui-reset/data-entry/checkbox/index.vue")
-  },
-  {
-    path: "/ui-reset/data-entry/search",
-    name: "search",
-    component: () => import(/* webpackChunkName: "exceptionModule" */"@/views/ui-reset/data-entry/search/index.vue")
-  },
-  {
-    path: "/ui-reset/data-entry/input",
-    name: "input",
-    component: () => import(/* webpackChunkName: "exceptionModule" */"@/views/ui-reset/data-entry/input/index.vue")
-  },
-  {
-    path: "/ui-reset/data-entry/uploader",
-    name: "uploader",
-    component: () => import(/* webpackChunkName: "exceptionModule" */"@/views/ui-reset/data-entry/uploader/index.vue")
-  },
-  {
-    path: "/ui-reset/data-entry/picker",
-    name: "picker",
-    component: () => import(/* webpackChunkName: "exceptionModule" */"@/views/ui-reset/data-entry/picker/index.vue")
-  },
-  {
-    path: "/ui-reset/data-display/noticebar",
-    name: "noticebar",
-    component: () => import(/* webpackChunkName: "exceptionModule" */"@/views/ui-reset/data-display/noticebar/index.vue")
-  },
-  {
-    path: "/ui-reset/data-display/list",
-    name: "list",
-    component: () => import(/* webpackChunkName: "exceptionModule" */"@/views/ui-reset/data-display/list/index.vue")
-  }
+
 ]
 
 
