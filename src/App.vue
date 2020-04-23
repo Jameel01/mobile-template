f3514c9f1717b8db2ae7ac51fd92c73f6a9dd75d
陈依婷 <1534136525@qq.com>
2020-04-23 09:44:42 +0800

    Revert "chore:修改路径和命名规则"
    This reverts commit b462796b91e575ce91a4c9aeba972ed03f8b761b.

--- src/App.vue
+++ src/App.vue
@@ -3,43 +3,43 @@
  * @Autor: guoruliang
  * @Date: 2020-04-03 16:52:52
  * @LastEditors: chenyt
- * @LastEditTime: 2020-04-22 09:44:42
+ * @LastEditTime: 2020-04-20 14:31:35
  -->
 <template>
-  <div id="app">
-    <y-loading :yLoading.sync='loadingStatus'></y-loading>
-    <!-- 页面切换容器 -->
-    <router-view />
-  </div>
+    <div id="app">
+        <y-loading :yLoading.sync='loadingStatus'></y-loading>
+        <!-- 页面切换容器 -->
+        <router-view />
+    </div>
 </template>
 <script>
 // import SessionUtil from "@/utils/sessionStorage";
 export default {
-  name: "app",
-  data() {
-    return {
-      loadingStatus: true //加载展示
-    }
-  },
-  created() {
-    // 隐藏加载
-    setTimeout(() => {
-      this.$bus.$emit("setLoading", false)
-    }, 2000)
-  },
-  methods: {}
+    name: "app",
+    data() {
+        return {
+            loadingStatus: true //加载展示
+        }
+    },
+    created() {
+        // 隐藏加载
+        setTimeout(() => {
+            this.$bus.$emit("setLoading", false)
+        }, 2000)
+    },
+    methods: {}
 }
 </script>
 
 <style lang="scss">
 #app {
-  background: #fff;
-  height: 100%;
+    background: #fff;
+    height: 100%;
 }
 html,
 body {
-  margin: 0;
-  padding: 0;
-  height: 100%;
+    margin: 0;
+    padding: 0;
+    height: 100%;
 }
 </style>
--- src/router/common/index.js
+++ src/router/common/index.js
@@ -3,67 +3,68 @@
  * @Autor: guoruliang
  * @Date: 2020-04-01 09:57:00
  * @LastEditors: chenyt
- * @LastEditTime: 2020-04-22 09:46:57
+ * @LastEditTime: 2020-04-20 13:57:08
  */
 
 export default [
-    {
-        path: "/",
-        name: "目录",
-        meta: {
-            needLogin: false
-        },
-        component: () => import(/* webpackChunkName: "loginModule" */"@/views/home.vue")
+  {
+    path: "/",
+    name: "目录",
+    meta: {
+      needLogin: false
     },
-    {
-        path: "/error",
-        name: "error",
-        component: () => import(/* webpackChunkName: "errorModule" */"@/views/error.vue")
+    component: () => import(/* webpackChunkName: "loginModule" */"@/views/Home.vue")
+  },
+  {
+    path: "/error",
+    name: "error",
+    component: () => import(/* webpackChunkName: "errorModule" */"@/views/Error.vue")
 
+  },
+  {
+    path: "/exception",
+    name: "exception",
+    meta: {
+      needLogin: false
     },
-    {
-        path: "/exception",
-        name: "exception",
-        meta: {
-            needLogin: false
-        },
-        component: () => import(/* webpackChunkName: "exceptionModule" */"@/views/example.vue")
-    },
+    component: () => import(/* webpackChunkName: "exceptionModule" */"@/views/example.vue")
+  },
 
-    {
-        path: "*",
-        name: "not-found",
-        component: () => import(/* webpackChunkName: "notFoundModule" */"@/views/404.vue")
-    },
-    {
-        path: "/uireset/dataEntry/button",
-        name: "button",
-        component: () => import(/* webpackChunkName: "exceptionModule" */"@/views/uireset/dataEntry/button.vue")
-    },
-    {
-        path: "/uireset/dataEntry/checkbox",
-        name: "checkbox",
-        component: () => import(/* webpackChunkName: "exceptionModule" */"@/views/uireset/dataEntry/checkbox.vue")
-    },
-    {
-        path: "/uireset/dataEntry/searchBar",
-        name: "searchBar",
-        component: () => import(/* webpackChunkName: "exceptionModule" */"@/views/uireset/dataEntry/searchBar.vue")
-    },
-    {
-        path: "/uireset/dataEntry/inputItem",
-        name: "inputItem",
-        component: () => import(/* webpackChunkName: "exceptionModule" */"@/views/uireset/dataEntry/inputItem.vue")
-    },
-    {
-        path: "/uireset/dataEntry/uploader",
-        name: "uploader",
-        component: () => import(/* webpackChunkName: "exceptionModule" */"@/views/uireset/dataEntry/uploader.vue")
-    },
-    {
-        path: "/uireset/dataEntry/picker",
-        name: "picker",
-        component: () => import(/* webpackChunkName: "exceptionModule" */"@/views/uireset/dataEntry/picker.vue")
-    }
+  {
+    path: "*",
+    name: "not-found",
+    component: () => import(/* webpackChunkName: "notFoundModule" */"@/views/404.vue")
+  },
+  {
+    path: "/button",
+    name: "button",
+    component: () => import(/* webpackChunkName: "exceptionModule" */"@/views/uireset/dataEntry/button.vue")
+  },
+  {
+    path: "/checkbox",
+    name: "checkbox",
+    component: () => import(/* webpackChunkName: "exceptionModule" */"@/views/uireset/dataEntry/checkbox.vue")
+  },
+  {
+    path: "/searchBar",
+    name: "searchBar",
+    component: () => import(/* webpackChunkName: "exceptionModule" */"@/views/uireset/dataEntry/searchBar.vue")
+  },
+  {
+    path: "/inputItem",
+    name: "inputItem",
+    component: () => import(/* webpackChunkName: "exceptionModule" */"@/views/uireset/dataEntry/inputItem.vue")
+  },
+  {
+    path: "/uploader",
+    name: "uploader",
+    component: () => import(/* webpackChunkName: "exceptionModule" */"@/views/uireset/dataEntry/uploader.vue")
+  },
+  {
+    path: "/picker",
+    name: "picker",
+    component: () => import(/* webpackChunkName: "exceptionModule" */"@/views/uireset/dataEntry/picker.vue")
+  }
 ]
+
 
