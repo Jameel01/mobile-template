7bc367c28fb91eac552bd247a9882b986bae90f7
郑小娜 <764427743@qq.com>
2020-04-26 17:38:05 +0800

    style：替换加载图片

--- src/components/global/y-loading/index.vue
+++ src/components/global/y-loading/index.vue
@@ -1,12 +1,20 @@
+<!--
+ * @Description: 
+ * @Version: 0.1
+ * @Autor: zhengxn
+ * @Date: 2020-04-24 13:57:10
+ * @LastEditors: zhengxn
+ * @LastEditTime: 2020-04-26 17:33:25
+ -->
 <template>
-    <div>
-        <!-- 加载gif -->
-        <div class="project-loading" v-show="yLoading">
-            <div class="loading-block">
-                <img src="@/assets/imgs/12333.gif" width="101px" />
-            </div>
-        </div>
+  <div>
+    <!-- 加载gif -->
+    <div class="project-loading" v-show="yLoading">
+      <div class="loading-block">
+        <img src="@/assets/imgs/loading.gif" width="101px" />
+      </div>
     </div>
+  </div>
 </template>
 <script>
 export default {
@@ -29,20 +37,20 @@ export default {
 <style lang='scss' scoped>
 // 加载框样式定义
 .project-loading {
-    background: #fbfbfb;
-    width: 100%;
-    height: 100%;
-    position: fixed;
-    left: 0;
-    top: 0;
-    z-index: 100000;
+  background: #fbfbfb;
+  width: 100%;
+  height: 100%;
+  position: fixed;
+  left: 0;
+  top: 0;
+  z-index: 100000;
 }
 
 .loading-block {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
 }
 </style>
 
--- src/views/login-module/login/index.vue
+++ src/views/login-module/login/index.vue
@@ -1,12 +1,18 @@
+<!--
+ * @Description: 
+ * @Version: 0.1
+ * @Autor: zhengxn
+ * @Date: 2020-04-24 13:44:52
+ * @LastEditors: zhengxn
+ * @LastEditTime: 2020-04-26 17:33:33
+ -->
 <template>
   <div>
-      是登录页
-      <img :src="require('@/assets/imgs/12333.gif')" alt="">
+    是登录页
+    <img :src="require('@/assets/imgs/loading.gif')" alt="">
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
