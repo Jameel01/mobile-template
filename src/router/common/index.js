f048eaf8ecd47271062a234be99457fa1642647f
郑小娜 <764427743@qq.com>
2020-05-06 17:00:12 +0800

    refactor: 空格更改

--- src/router/common/index.js
+++ src/router/common/index.js
@@ -7,20 +7,20 @@
  */
 
 export default [
-	{
-		path: "/",
-		name: "目录",
-		meta: {
-			needLogin: false
-		},
-		component: () => import(/* webpackChunkName: "loginModule" */"@/views/home.vue")
-	},
+  {
+    path: "/",
+    name: "目录",
+    meta: {
+      needLogin: false
+    },
+    component: () => import(/* webpackChunkName: "loginModule" */"@/views/home.vue")
+  },
 
-	{
-		path: "*",
-		name: "not-found",
-		component: () => import(/* webpackChunkName: "notFoundModule" */"@/views/default.vue")
-	}
+  {
+    path: "*",
+    name: "not-found",
+    component: () => import(/* webpackChunkName: "notFoundModule" */"@/views/default.vue")
+  }
 
 ]
 
