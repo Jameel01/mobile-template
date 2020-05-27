c3bbc91f2fc054c46250cd99c66bbd0b571ab320
陈依婷 <1534136525@qq.com>
2020-05-27 11:52:00 +0800

    Revert "柔柔弱弱"
    This reverts commit a6c52d91bb006f73ed92e699fb703c0b05a03ba7.

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
  * @Autor: guoruliang
  * @Date: 2020-04-03 16:52:52
  * @LastEditors: chenyt
- * @LastEditTime: 2020-05-27 11:50:30
+ * @LastEditTime: 2020-05-06 14:10:28
  -->
 <template>
   <div id="app">
@@ -16,7 +16,6 @@ export default {
   name: "app",
   data() {
     return {
-      loadingStatus: true, //加载展示
       loadingStatus: true //加载展示
     }
   },
