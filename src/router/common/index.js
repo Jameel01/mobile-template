baa646b183753321fe2ede9e8c66f2ebcd05e069
陈依婷 <1534136525@qq.com>
2020-05-29 14:04:43 +0800

    feat: 新增记住滚动条，缓存设置

--- src/router/common/index.js
+++ src/router/common/index.js
@@ -3,24 +3,25 @@
  * @Autor: guoruliang
  * @Date: 2020-04-01 09:57:00
  * @LastEditors: chenyt
- * @LastEditTime: 2020-04-28 16:57:20
+ * @LastEditTime: 2020-05-29 13:52:08
  */
 
 export default [
-  {
-    path: "/",
-    name: "目录",
-    meta: {
-      needLogin: false
-    },
-    component: () => import(/* webpackChunkName: "loginModule" */"@/views/home.vue")
-  },
+	{
+		path: "/",
+		name: "目录",
+		meta: {
+			needLogin: false,
+			keepAlive: true
+		},
+		component: () => import(/* webpackChunkName: "loginModule" */"@/views/home.vue")
+	},
 
-  {
-    path: "*",
-    name: "not-found",
-    component: () => import(/* webpackChunkName: "notFoundModule" */"@/views/default.vue")
-  }
+	{
+		path: "*",
+		name: "not-found",
+		component: () => import(/* webpackChunkName: "notFoundModule" */"@/views/default.vue")
+	}
 
 ]
 
