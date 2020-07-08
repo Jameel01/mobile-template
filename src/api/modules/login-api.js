bc835b2e20e901d4904477ee58ae2621fd93ed4a
root <chenyiting@ylzinfo.com>
2020-07-08 14:01:31 +0800

    chore: 规范api请求方式

--- src/api/modules/login-api.js
+++ src/api/modules/login-api.js
@@ -2,8 +2,8 @@
  * @Description: 按照功能模块划分接口
  * @Autor: chenyt
  * @Date: 2020-03-22 12:03:43
- * @LastEditors: chenyt
- * @LastEditTime: 2020-05-09 15:25:51
+ * @LastEditors: Chenyt
+ * @LastEditTime: 2020-07-08 14:00:37
  */
 
 import request from "../axios"
@@ -18,7 +18,5 @@ import request from "../axios"
 export const prbAdd = function(params = {}, options = {}) {
   return request.post("/base/api/prb/add", {
     data: params
-  }, options).then(result => {
-    return result.data
-  })
+  }, options)
 }
