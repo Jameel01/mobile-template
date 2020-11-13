a8075c41515780a89270c7d456883fb08ec9c69e
root <chenyiting@ylzinfo.com>
2020-11-13 15:36:40 +0800

    fix: 去除hash监听

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
  * @Autor: guoruliang
  * @Date: 2020-04-03 16:52:52
  * @LastEditors: Chenyt
- * @LastEditTime: 2020-10-30 15:36:31
+ * @LastEditTime: 2020-11-13 15:36:31
  -->
 <template>
   <div id="app">
@@ -23,9 +23,9 @@ export default {
     }
   },
   mounted(){
-    window.addEventListener("hashchange", () => {
-      this.$router.go(0)
-    })
+    // window.addEventListener("hashchange", () => {
+    //   this.$router.go(0)
+    // })
   },
   created() {
     this.$Loading.show(2000)
