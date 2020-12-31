fd57a6b774e12b0b6f72b5c51739fd3842eb5f2b
智宇星空 <328054241@qq.com>
2020-12-31 11:39:50 +0800

    fix: 导入全局filters

--- src/filters/index.js
+++ src/filters/index.js
@@ -6,16 +6,6 @@
  * @Last Modified time: 2020-03-20 15:18:22
  */
 
-/**
- * @Descripttion: 数据字典过滤器
- * @param {val为数据值,status为数据}
- * @return: 数据编码对应的数据名
- * @Author: syx
- */
-export function dictFormat(val, status) {
-  return !status ? "" : status.find(item => { return item.dicCode === val }) ? status.find(item => { return item.dicCode === val }).dicCodeName : ""
-}
-
 export {
   getCodeByLabel,
   getCodeByLabels,
@@ -25,4 +15,4 @@ export {
 
 export { dayFormat } from "@/utils/dayjs"
 import desensitization from "@/utils/desensitization"
-export { desensitization } 
+export { desensitization }
--- src/main.js
+++ src/main.js
@@ -2,23 +2,30 @@
  * @Description: 主入口文件
  * @Autor: guoruliang
  * @Date: 2020-04-08 09:32:12
- * @LastEditors: Chenyt
- * @LastEditTime: 2020-11-13 13:57:04
+ * @LastEditors: yjm
+ * @LastEditTime: 2020-12-31 11:39:28
  */
 import Vue from "vue"
 import App from "@/App.vue"
 import Router from "vue-router"
 import router from "@/router"
+
 import { Toast } from "@ylz/vant"
+Vue.use(Toast)
+
 import Mixin from "./mixins"
 Vue.mixin(Mixin)
+
+import * as filters from "./filters"
+Object.keys(filters).map(item => {
+  Vue.filter(item, filters[item])
+})
+
 import pxToViewport from "@/utils/px-to-viewport"
 Vue.prototype.$pxToViewport = pxToViewport
 
-Vue.use(Toast)
 // 引入全局iconfont
 import "@/styles/iconfonts/iconfont.css"
-
 // 引入全部样式
 import "@ylz/vant/lib/index.less"
 // 自动注册全局的组件
@@ -29,10 +36,13 @@ import "@/components/index"
 import "@/styles/common.less"
 //先关闭医保规范样式文件
 // import "@/styles/theme/reset-vant.less"
+
 //vuex存储
 import store from "@/store"
+
 //引入常用的vant组件
 import "@/use"
+
 //引入路由处理方法
 import { beforeEachHandler, beforeEachToExample } from "@/router/before-each"
 //设置路由标题
@@ -43,12 +53,14 @@ router.beforeEach(beforeEachToExample)
 router.beforeEach(beforeEachHandler)
 //标题设置
 router.afterEach(afterEachHandler)
+
 //sessionstorage引入
 import SessionUtil from "@/utils/session-storage"
 // 挂载工具
 Vue.prototype.$sessionUtil = SessionUtil
 //全局toast提示
 Vue.prototype.$toast = Toast
+
 // 修改路由跳转报错的bug-start
 const routerPush = Router.prototype.push
 Router.prototype.push = function push(location) {
@@ -65,6 +77,7 @@ Object.defineProperties(Vue.prototype, {
     }
   }
 })
+
 Vue.config.devtools = true
 Vue.config.productionTip = false
 
@@ -72,6 +85,7 @@ Vue.config.productionTip = false
 if (module && module.hot) {
   module.hot.accept()
 }
+
 window.gvm = new Vue({
   router,
   store,
