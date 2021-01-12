0742550f8dde4645c2e7f61a53142a1e2f8158b0
root <chenyiting@ylzinfo.com>
2021-01-12 12:48:14 +0800

    feat: 添加函数

--- src/utils/common.js
+++ src/utils/common.js
@@ -62,7 +62,25 @@ export  function jsonToArray(data, value = "label", key = "value") {
 
   return arr
 }
-
+/**
+ * @param {string} url
+ * @returns {Object}
+ */
+export function param2Obj(url) {
+  const search = url.split('?')[1]
+  if (!search) {
+    return {}
+  }
+  return JSON.parse(
+    '{"' +
+    decodeURIComponent(search)
+      .replace(/"/g, '\\"')
+      .replace(/&/g, '","')
+      .replace(/=/g, '":"')
+      .replace(/\+/g, ' ') +
+    '"}'
+  )
+}
 /**
 * 设置页面标题
 * @param {String} title 标题
